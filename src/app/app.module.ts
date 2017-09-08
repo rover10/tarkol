3e9904622569823c0f7a69491efa401e66cd5e93
Rakesh Kumar <rakesh.r1303@gmail.com>
2017-09-09 03:45:17 +0530

    Server updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,30 @@ import { AppComponent } from './app.component';
 import {TopBarComponent} from './bars/top/topbar.component';
 import {LeftMenuBarComponent} from './bars/left/leftMenuBar.component'; 
 
+import { HttpClientModule } from '@angular/common/http';
+
 import {DropdownButtonComponent} from './menu/drop-down/dropdown.component';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import{Terminal} from './terminal/terminal.component';
 import { AngularDraggableModule } from 'angular2-draggable';
 import {ResizableModule} from 'angular2-resizable';
-
+import { HttpModule }    from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import 'rxjs/add/operator/toPromise';
+
 
 @NgModule({
   declarations: [
     AppComponent, TopBarComponent, LeftMenuBarComponent,DropdownButtonComponent,Terminal
   ],
   imports: [
-    BrowserModule, NgbModule.forRoot(), AngularDraggableModule,ResizableModule,BrowserAnimationsModule,
+    BrowserModule, NgbModule.forRoot(), AngularDraggableModule,ResizableModule,BrowserAnimationsModule, HttpModule,
+    
   ],
-  providers: [InteractionService],
+  providers: [InteractionService,  ],
   bootstrap: [AppComponent, TopBarComponent,LeftMenuBarComponent,DropdownButtonComponent,Terminal ]
 })
 export class AppModule { }
--- src/app/bars/top/topbar.component.ts
+++ src/app/bars/top/topbar.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {DropdownButtonComponent} from './../../menu/drop-down/dropdown.component';
 import {InteractionService} from './../../services/component-interaction/interaction.service';
+import { HttpModule }    from '@angular/http';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'top-bar',
@@ -9,11 +14,26 @@ import {InteractionService} from './../../services/component-interaction/interac
 })
 
 export class TopBarComponent {
+  
     toggle_main_pane :boolean = false; 
-    constructor(private updateService: InteractionService) {}
+    constructor(private updateService: InteractionService, private http: Http) {}
     changeOutside() {
       this.toggle_main_pane =!this.toggle_main_pane;
       this.updateService.changeData(this.toggle_main_pane);
     }
     title = 'app';
+
+    newProject(){
+      console.log('Sending get request');
+      this.http.get(`topics/1`)
+      .toPromise()
+      .then(response => {console.log(response.json()); alert(response.json())})
+      .catch(this.handleError);
+
+   }
+
+    private handleError(error: any): Promise<any> {
+      console.error('An error occurred', error); // for demo purposes only
+      return Promise.reject(error.message || error);
+    }
 }
